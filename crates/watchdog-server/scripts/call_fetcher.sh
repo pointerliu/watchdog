d63a998f234e62d1991dd09a1c30bbf4c0ad1742
pointerliu <zizhen_liu@nudt.edu.cn>
2025-08-30 10:55:36 +0800

    server: add fetcher test script

---
+++ crates/watchdog-server/scripts/call_fetcher.sh
@@ -0,0 +1,10 @@
+curl -X GET 127.0.0.1:8080 && echo "\n"
+curl -X GET "127.0.0.1:8080/api/v1/fetchers/types" && echo "\n"
+
+curl -X POST "127.0.0.1:8080/api/v1/fetchers" \
+  -H "Content-Type: application/json" \
+  -d '{"user_id": "lzz", "fetcher_name": "fetcher_1", "fetcher_type": "ArxivFetcher"}' && echo "\n"
+
+curl -X GET "127.0.0.1:8080/api/v1/fetchers/lzz" && echo "\n"
+curl -X DELETE "127.0.0.1:8080/api/v1/fetchers/lzz/fetcher_1" && echo "\n"
+curl -X GET "127.0.0.1:8080/api/v1/fetchers/lzz" && echo "\n"
